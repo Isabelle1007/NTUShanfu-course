fc7b917f100688bdad9a61672fe17dcb7f26bbaa
isabelle <isa20202020@gmail.com>
2023-05-16 11:07:30 +0800

    fix: 🐛 consider co-work of one curriculum

--- backend/models/curriculum_model.js
+++ backend/models/curriculum_model.js
@@ -3,64 +3,73 @@ const pool = require('../db')
 const { changeDataFormat } = require('../utils/util')
 
 const getAllCurriculum = async () => {
-    let curr = []
-    const query = `SELECT c.*, u.u_name, h.h_name, t.t_name, f.url 
-                   FROM curricula c JOIN users u ON c.author_id = u.id 
-                                    JOIN homes h ON c.home_id = h.id 
-                                    JOIN types t ON c.type_id = t.id 
-                                    LEFT JOIN files f ON c.file_id = f.id`
+    let curr = [];
+    const query = `SELECT c.id, c.title, GROUP_CONCAT(u.u_name) AS authors, c.semester, h.h_name, t.t_name, f.url, c.created_at
+                   FROM curricula c 
+                   JOIN user_curriculum u_c ON c.id = u_c.cid 
+                   JOIN users u ON u_c.uid = u.id 
+                   JOIN homes h ON c.home_id = h.id 
+                   JOIN types t ON c.type_id = t.id 
+                   LEFT JOIN files f ON c.file_id = f.id
+                   GROUP BY c.id, c.title, c.semester, h.h_name, t.t_name, f.url, c.created_at`;
     const [result] = await pool.execute(query);
-    try{
-        if(result.length > 0){
-            for(var i = 0; i < result.length; i++){
-                let date = changeDataFormat(result[i].created_at)
+    try {
+        if (result.length > 0) {
+            for (var i = 0; i < result.length; i++) {
+                let date = changeDataFormat(result[i].created_at);
+                let authors = result[i].authors.split(","); // Split the authors string into an array
                 let curriculum = {
                     "id": result[i].id,
                     "title": result[i].title,
-                    "author": result[i].u_name,
+                    "author": authors,
                     "semester": result[i].semester,
                     "home": result[i].h_name,
                     "type": result[i].t_name,
                     "file": result[i].url,
                     "created": date
-                }
-                curr.push(curriculum)
+                };
+                curr.push(curriculum);
             }
         }
-        if(curr.length > 0){
+        if (curr.length > 0) {
             return {
                 "message": "Success",
                 "code": "000",
                 "data": curr
-            }
-        }else{
+            };
+        } else {
             return {
-                "message": "No Currirula",
+                "message": "No Curriculum",
                 "code": "001"
-            }
+            };
         }
-    }catch(err){
-        return err
+    } catch (err) {
+        return err;
     }
 };
 
+
 const getCurriculumWithType = async (type) => {
     let curr = []
-    const query = `SELECT c.*, u.u_name, h.h_name, t.t_name, f.url 
-                   FROM curricula c JOIN users u ON c.author_id = u.id 
-                                    JOIN homes h ON c.home_id = h.id 
-                                    JOIN types t ON c.type_id = t.id 
-                                    LEFT JOIN files f ON c.file_id = f.id
-                   WHERE t.t_name = '${type}'`
+    const query = `SELECT c.id, c.title, GROUP_CONCAT(u.u_name) AS authors, c.semester, h.h_name, t.t_name, f.url, c.created_at
+                   FROM curricula c 
+                   JOIN user_curriculum u_c ON c.id = u_c.cid 
+                   JOIN users u ON u_c.uid = u.id 
+                   JOIN homes h ON c.home_id = h.id 
+                   JOIN types t ON c.type_id = t.id 
+                   LEFT JOIN files f ON c.file_id = f.id
+                   WHERE t.t_name = '${type}'
+                   GROUP BY c.id, c.title, c.semester, h.h_name, t.t_name, f.url, c.created_at`
     const [result] = await pool.execute(query);
     try{
         if(result.length > 0){
             for(var i = 0; i < result.length; i++){
-                let date = changeDataFormat(result[i].created_at)                
+                let date = changeDataFormat(result[i].created_at) 
+                let authors = result[i].authors.split(","); // Split the authors string into an array               
                 let curriculum = {
                     "id": result[i].id,
                     "title": result[i].title,
-                    "author": result[i].u_name,
+                    "author": authors,
                     "semester": result[i].semester,
                     "home": result[i].h_name,
                     "type": result[i].t_name,
@@ -88,47 +97,52 @@ const getCurriculumWithType = async (type) => {
 };
 
 const getCurriculumByUserId = async (id) => {
-    let curr = []
-    const query = `SELECT c.*, u.u_name, h.h_name, t.t_name, f.url 
-                   FROM curricula c JOIN users u ON c.author_id = u.id 
-                                    JOIN homes h ON c.home_id = h.id 
-                                    JOIN types t ON c.type_id = t.id 
-                                    LEFT JOIN files f ON c.file_id = f.id
-                   WHERE u.id = '${id}'`
+    let curr = [];
+    const query = `SELECT c.id, c.title, 
+                  (SELECT GROUP_CONCAT(u_name) FROM users WHERE id IN (SELECT uid FROM user_curriculum WHERE cid = c.id)) AS authors,
+                   c.semester, h.h_name, t.t_name, f.url, c.created_at
+                   FROM curricula c 
+                   JOIN homes h ON c.home_id = h.id 
+                   JOIN types t ON c.type_id = t.id 
+                   LEFT JOIN files f ON c.file_id = f.id
+                   WHERE c.id IN (SELECT cid FROM user_curriculum WHERE uid = '${id}')
+                   GROUP BY c.id, c.title, c.semester, h.h_name, t.t_name, f.url, c.created_at`;
     const [result] = await pool.execute(query);
-    try{
-        if(result.length > 0){
-            for(var i = 0; i < result.length; i++){
-                let date = changeDataFormat(result[i].created_at)
+    try {
+        if (result.length > 0) {
+            for (var i = 0; i < result.length; i++) {
+                let date = changeDataFormat(result[i].created_at);
+                let authors = result[i].authors ? result[i].authors.split(",") : []; // Split the authors string into an array or assign an empty array if authors is null
                 let curriculum = {
                     "id": result[i].id,
                     "title": result[i].title,
-                    "author": result[i].u_name,
+                    "author": authors,
                     "semester": result[i].semester,
                     "home": result[i].h_name,
                     "type": result[i].t_name,
                     "file": result[i].url,
                     "created": date
-                }
-                curr.push(curriculum)
+                };
+                curr.push(curriculum);
             }
         }
-        if(curr.length > 0){
+        if (curr.length > 0) {
             return {
                 "message": "Success",
                 "code": "000",
                 "data": curr
-            }
-        }else{
+            };
+        } else {
             return {
-                "message": "No Currirula",
+                "message": "No Curriculum",
                 "code": "001"
-            }
+            };
         }
-    }catch(err){
-        return err
+    } catch (err) {
+        return err;
     }
 };
+
 
 module.exports = {
     getAllCurriculum,
