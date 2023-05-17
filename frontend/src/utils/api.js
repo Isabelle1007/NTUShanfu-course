fd39195ecd302513abbfeb8461d58dce4a890d9d
isabelle <isa20202020@gmail.com>
2023-05-17 22:55:56 +0800

    fix: 🐛 change API endpoint for fetch()

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,9 +3,9 @@ export const api = {
     hostname_be: 'http://localhost:4000',
     hostname_fe: 'http://localhost:5173',
     getAllHomes() {
-        return fetch(`${this.hostname_be}/home/all`).then(response => response.json());
+        return fetch(`${this.hostname_be}/api/home/all`).then(response => response.json());
     },
     getAllTypes() {
-        return fetch(`${this.hostname_be}/type/all`).then(response => response.json());
+        return fetch(`${this.hostname_be}/api/type/all`).then(response => response.json());
     },
 };
