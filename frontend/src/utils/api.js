7cb3f4034e421865a5299b22bd933da2ce9fc934
isabelle <isa20202020@gmail.com>
2023-05-22 13:29:03 +0800

    fix: 🐛 change port and edit vite.config

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 export const api = {
     // hostname: 'http://18.182.163.170',
-    hostname_be: 'http://localhost:4002',
-    hostname_fe: 'http://localhost:5173',
+    hostname_be: 'http://localhost:4000',
+    hostname_fe: 'http://localhost:3000',
     getAllHomes() {
         return fetch(`${this.hostname_be}/api/home/all`).then(response => response.json());
     },
