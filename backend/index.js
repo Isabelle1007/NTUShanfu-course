b0786f68a598c98975b1eaf4b286f165e7a8496b
Huang Chen Syuan <chensyuan.huang@gmail.com>
2023-12-24 22:27:22 -0600

    style: 💄 Restructure index.js

--- backend/index.js
+++ backend/index.js
@@ -2,33 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 const { PORT } = process.env;
 
 const app = express()
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+// Use CORS and JSON middleware
 app.use(cors())
 app.use(express.json())
 
-// Router
+// Define routes
 const basicRouter = require('./routes/basic_route');
-app.use('/', basicRouter);
-
 const curriculumRouter = require('./routes/curriculum_route');
-app.use('/curriculum', curriculumRouter);
-
 const userRouter = require('./routes/user_route');
-app.use('/user', userRouter);
-
 const typeRouter = require('./routes/type_route');
-app.use('/type', typeRouter);
-
 const homeRouter = require('./routes/home_route');
-app.use('/home', homeRouter);
-
 const roleRouter = require('./routes/role_route');
-app.use('/role', roleRouter);
-
 const fileRouter = require('./routes/file_route');
+
+app.use('/', basicRouter);
+app.use('/curriculum', curriculumRouter);
+app.use('/user', userRouter);
+app.use('/type', typeRouter);
+app.use('/home', homeRouter);
+app.use('/role', roleRouter);
 app.use('/file', fileRouter);
 
 // Listen on server port
