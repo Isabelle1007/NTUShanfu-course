e8ae16f02688ef4f86e57a8a1b3edb2a0baef0a2
Huang Chen Syuan <chensyuan.huang@gmail.com>
2023-12-25 09:15:35 -0600

    style: 💄 Remove fe URL in api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,7 @@
 export const api = {
     // hostname_be: 'http://18.182.163.170/api',
-    // hostname_fe: 'http://18.182.163.170',
     hostname_be: 'http://ntushanfu-search-curricula.onrender.com',
     // hostname_be: 'http://localhost:4000',
-    hostname_fe: 'http://localhost:3000',
    
     getAllHomes() {
         return fetch(`${this.hostname_be}/home/all`).then(response => response.json());
