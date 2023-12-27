f291551acfeb122f29a25041e006197390a8838a
Huang Chen Syuan <chensyuan.huang@gmail.com>
2023-12-26 18:36:58 -0600

    fix: 🐛 Remove editing db for other files

--- backend/controllers/file_controller.js
+++ backend/controllers/file_controller.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const multer = require('multer');
-const { s3Uploadv2 } = require('../utils/s3Service');
-const { getInfoOfUser, updateInfoByEmail } = require('../models/user_model');
+const { s3Uploadv2, deleteFileFromS3 } = require('../utils/s3Service');
+const { getInfoByUserEmail, updateInfoByEmail } = require('../models/user_model');
 const File = require('../models/file_model');
 
 const storage = multer.memoryStorage();
@@ -70,17 +70,42 @@ const postFile = async (req, res) => {
             const fileType = type === 'word' ? 'docx' : (type === 'pdf' ? 'pdf' : req.body.format);
             const result = await s3Uploadv2(req.file, req.body.name, fileType);
             if (result.code === '000') {
-                const url = result.data.response.Location;
-                const updateDb = await updateInfoByEmail(req.body.user_email, { "picture_url": url });
-                if (updateDb.code === '000') {
+                if(type === 'image'){
+                    const url = result.data.response.Location;
+                    const userInfo = await getInfoByUserEmail(req.body.user_email);
+                    let old_url
+                    if(userInfo.code === '000'){
+                        old_url = userInfo.data.picture_url;
+                    }else{
+                        res.json({
+                            "message": userInfo.message,
+                            "code": "999"
+                        });
+                    }
+                    const updateDb = await updateInfoByEmail(req.body.user_email, { "picture_url": url });
+                    if (updateDb.code === '000') {
+                        const deleteOldPic = await deleteFileFromS3(old_url);
+                        if(deleteOldPic.code === '000'){
+                            res.json({
+                                "message": "Success",
+                                "code": "000",
+                            });
+                        }else{
+                            res.json({
+                                "message": deleteOldPic.message,
+                                "code": "999"
+                            });
+                        }
+                    } else {
+                        res.json({
+                            "message": updateDb.message,
+                            "code": "999"
+                        });
+                    }
+                }else{
                     res.json({
                         "message": "Success",
                         "code": "000",
-                    });
-                } else {
-                    res.json({
-                        "message": updateDb.message,
-                        "code": "999"
                     });
                 }
             } else {
