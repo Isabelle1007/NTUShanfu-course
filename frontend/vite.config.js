b36aa72e467c135331c9fdcd59d02b30e32c7fee
Huang Chen Syuan <chensyuan.huang@gmail.com>
2023-12-27 17:34:18 -0600

    fix: 🐛 Add rollup config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,5 +11,10 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-pdf'],
+    },
   }
 });
