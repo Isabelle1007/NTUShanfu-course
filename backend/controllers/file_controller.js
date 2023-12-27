c1ec3a83fcf01d6dc3ee445dabf7f811c210873f
Huang Chen Syuan <chensyuan.huang@gmail.com>
2023-12-27 16:08:46 -0600

    fix: 🐛 Remove the code to delete replicable images on s3

--- backend/controllers/file_controller.js
+++ backend/controllers/file_controller.js
@@ -68,34 +68,45 @@ const postFile = async (req, res) => {
     try {
         await multerUpload(uploadType)(req, res, async () => {
             const fileType = type === 'word' ? 'docx' : (type === 'pdf' ? 'pdf' : req.body.format);
-            const result = await s3Uploadv2(req.file, req.body.name, fileType);
-            if (result.code === '000') {
+            const uploadFileToS3 = await s3Uploadv2(req.file, req.body.name, fileType);
+            if (uploadFileToS3.code === '000') {
                 if(type === 'image'){
-                    const url = result.data.response.Location;
-                    const userInfo = await getInfoByUserEmail(req.body.user_email);
-                    let old_url
-                    if(userInfo.code === '000'){
-                        old_url = userInfo.data.picture_url;
-                    }else{
-                        res.json({
-                            "message": userInfo.message,
-                            "code": "999"
-                        });
-                    }
+                    const url = uploadFileToS3.data.response.Location;
+                    // const userInfo = await getInfoByUserEmail(req.body.user_email);
+                    // let old_url
+                    // if(userInfo.code === '000'){
+                    //     old_url = userInfo.data.picture_url; 
+                    // }else{
+                    //     res.json({
+                    //         "message": userInfo.message,
+                    //         "code": "999"
+                    //     });
+                    // }
                     const updateDb = await updateInfoByEmail(req.body.user_email, { "picture_url": url });
                     if (updateDb.code === '000') {
-                        const deleteOldPic = await deleteFileFromS3(old_url);
-                        if(deleteOldPic.code === '000'){
-                            res.json({
-                                "message": "Success",
-                                "code": "000",
-                            });
-                        }else{
-                            res.json({
-                                "message": deleteOldPic.message,
-                                "code": "999"
-                            });
-                        }
+                        res.json({
+                            "message": updateDb.message,
+                            "code": "000"
+                        });
+                        // if(old_url != default_picture_url){
+                        //     // const deleteOldPic = await deleteFileFromS3(old_url);
+                        //     // if(deleteOldPic.code === '000'){
+                        //     //     res.json({
+                        //     //         "message": "Success",
+                        //     //         "code": "000",
+                        //     //     });
+                        //     // }else{
+                        //     //     res.json({
+                        //     //         "message": deleteOldPic.message,
+                        //     //         "code": "999"
+                        //     //     });
+                        //     // }
+                        // }else{
+                        //     res.json({
+                        //         "message": updateDb.message,
+                        //         "code": "000"
+                        //     });
+                        // }
                     } else {
                         res.json({
                             "message": updateDb.message,
@@ -106,12 +117,12 @@ const postFile = async (req, res) => {
                     res.json({
                         "message": "Success",
                         "code": "000",
-                        "data": result.data
+                        "data": uploadFileToS3.data
                     });
                 }
             } else {
                 res.json({
-                    "message": result.message,
+                    "message": uploadFileToS3.message,
                     "code": "999"
                 });
             }
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,10 @@
 export const api = {
     //production
-    server_url: 'https://ntushanfu-search-curricula.onrender.com', 
+    server_url: 'https://ntushanfu-search-curricula.onrender.com',
     app_url: 'https://ntushanfu.onrender.com',
     //development
     // server_url: 'http://localhost:4000',
-    // app_url: 'http://localhost:3000', 
+    // app_url: 'http://localhost:3000',
 
     getAllHomes() {
         return fetch(`${this.server_url}/home/all`).then(response => response.json());
