935c9de22fedea0244aa7d85223f864a9d0ab8d4
Huang Chen Syuan <chensyuan.huang@gmail.com>
2024-01-04 14:35:07 -0600

    fix: 🐛 Change pic

--- backend/controllers/file_controller.js
+++ backend/controllers/file_controller.js
@@ -72,38 +72,31 @@ const postFile = async (req, res) => {
             if (uploadFileToS3.code === '000') {
                 if(type === 'image'){
                     const url = uploadFileToS3.data.response.Location;
-                    // const userInfo = await getInfoByUserEmail(req.body.user_email);
-                    // let old_url
-                    // if(userInfo.code === '000'){
-                    //     old_url = userInfo.data.picture_url; 
-                    // }else{
-                    //     res.json({
-                    //         "message": userInfo.message,
-                    //         "code": "999"
-                    //     });
-                    // }
+                    const userInfo = await getInfoByUserEmail(req.body.user_email);
+                    let old_url
+                    if(userInfo.code === '000'){
+                        old_url = userInfo.data.picture_url; 
+                    }else{
+                        res.json({
+                            "message": userInfo.message,
+                            "code": "999"
+                        });
+                    }
                     const updateDb = await updateInfoByEmail(req.body.user_email, { "picture_url": url });
                     if (updateDb.code === '000') {
                         res.json(updateDb)
-                        // if(old_url != default_picture_url){
-                        //     // const deleteOldPic = await deleteFileFromS3(old_url);
-                        //     // if(deleteOldPic.code === '000'){
-                        //     //     res.json({
-                        //     //         "message": "Success",
-                        //     //         "code": "000",
-                        //     //     });
-                        //     // }else{
-                        //     //     res.json({
-                        //     //         "message": deleteOldPic.message,
-                        //     //         "code": "999"
-                        //     //     });
-                        //     // }
-                        // }else{
-                        //     res.json({
-                        //         "message": updateDb.message,
-                        //         "code": "000"
-                        //     });
-                        // }
+                        const deleteOldPic = await deleteFileFromS3(old_url);
+                        if(deleteOldPic.code === '000'){
+                            res.json({
+                                "message": "Success",
+                                "code": "000",
+                            });
+                        }else{
+                            res.json({
+                                "message": deleteOldPic.message,
+                                "code": "999"
+                            });
+                        }
                     } else {
                         res.json(updateDb)
                     }
